ce3562d2ca9d1a20ec18954906c0f1afa119d0fa
Patryk <cwikowskipatryk@gmail.com>
2016-11-24 01:39:35 +0100

    Commit 3. Praca nad teksturami mapy, modelowanie mapy, stworzenie modelu drzwi

---
+++ Assets/Skrypty/Wystrzal.js
@@ -0,0 +1,9 @@
+﻿function Update () 
+{
+	if(Input.GetButtonDown("Fire1")) 
+	{
+		var gunsound : AudioSource = GetComponent.<AudioSource>();
+		gunsound.Play();
+		GetComponent.<Animation>().Play("Colt 1911 wystrzal");
+	}
+}
