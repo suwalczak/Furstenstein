a51cd1023b7d2296c0b3c126a91cc068210c8a80
phtms <xxmarcoxx@o2.pl>
2016-11-29 18:45:52 +0100

    Update
    Dodałem:
    - Monety
    - Zbieranie
    - wstępne UI

---
+++ Assets/CollectionGold.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CollectionGold : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+        Points.score++;
+		Destroy(this.gameObject);
+
+	}
+}
---
+++ Assets/Points.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour {
+    public static int score;
+    Text goldText;    
+
+	// Use this for initialization
+	void Start () {
+        goldText = GetComponent<Text>();
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        goldText.text = "" + score;
+	}
+
+
+
+}
