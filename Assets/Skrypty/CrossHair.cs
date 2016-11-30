2c0f0302115accbc846705ecbea843cdc39509a6
phtms <xxmarcoxx@o2.pl>
2016-11-30 12:17:05 +0100

    s
    s

---
+++ Assets/Skrypty/CrossHair.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossHair : MonoBehaviour
+{
+
+    public Texture crosshair;
+
+    public int offsetw;
+    public int offseth;
+    public int divide = 1;
+
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(Screen.width / 2 - offsetw, Screen.height / 2 - offseth, crosshair.width / divide, crosshair.height / divide), crosshair);
+    }
+}
