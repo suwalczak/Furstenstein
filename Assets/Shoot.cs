d4451f6fa90df755a05b894090decc54bcf49637
Czarek_Dampc <czak27@o2.pl>
2016-12-05 14:41:54 +0100

    modele
    modele

---
+++ Assets/Shoot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject bullet;
+    public float delayTime = 8;
+   
+    private float counter = 0;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.Mouse0)&& counter > delayTime)
+        {
+            Instantiate(bullet, transform.position, transform.rotation);
+           
+            counter = 0;    
+        }
+        counter += Time.deltaTime;
+	}
+}
